39de4fdcc6bf07e76f728f761ff48d5c0e4991db
Zishan Shaikh <100702557+zishan29@users.noreply.github.com>
2024-05-12 14:57:41 +0530

    Change the put controller of blogs

--- controllers/blog.js
+++ controllers/blog.js
@@ -38,14 +38,7 @@ blogRouter.delete('/:id', async (request, response) => {
 });
 
 blogRouter.put('/:id', async (request, response) => {
-  const body = request.body;
-
-  const blog = {
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-  };
+  const blog = request.body;
 
   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
     new: true,
